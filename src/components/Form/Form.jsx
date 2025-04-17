b96c4e2e0aa6f0d79cb7db66d944786cbc855115
tlamngu <tlamngu@gmail.com>
2025-04-17 14:22:42 +0700

    Updated API.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useResponsive } from "../Layout/ResponsiveProvider";
+import axios from "axios";
 
 function Form() {
   const { isMobile, globalWidth } = useResponsive();
@@ -9,41 +10,54 @@ function Form() {
     UserTelForm: "",
     Aera: "",
     Service: "",
+    ip: "",
+    url: window.location.href, // Lấy URL hiện tại
   });
+
+  useEffect(() => {
+    axios.get("https://api.ipify.org?format=json")
+      .then(response => {
+        setFormData(prevState => ({ ...prevState, ip: response.data.ip }));
+      })
+      .catch(error => {
+        console.error("Không thể lấy địa chỉ IP:", error);
+      });
+  }, []);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const postData = (data)=>{
-    // const myHeaders = new Headers();
-    const API = `https://script.google.com/macros/s/AKfycbzggVMjcgZe2CVSJwlr_ZpRONJhCbKnzLTiwWGj0FdgGdcOqcdNmB4CDEPmzVH-wBI/exec`
-    // Example fetch request
-fetch(`${API}?name=${data.name}&email=${data.email}&phone=${data.phone}&country=${data.country}&service=${data.service}`, {
-  method: 'GET',
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-  }
+  const postData = (data) => {
+    
+    const API = `https://script.google.com/macros/s/AKfycbyQFA1rAlIRWl0b-J-hj6WZmWABe6D5xxKoWxWmOVfdp_97tHgr-JhqoGgzxqBffEOH/exec`;
+
+    fetch(`${API}?name=${data.name}&email=${data.email}&phone=${data.phone}&country=${data.country}&service=${data.service}&ip=${data.ip}&durl=${encodeURIComponent(data.url)}`, {
+      method: "GET",
+      followRedirects: true,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted:", formData);
-    postData(
-      {
-        name: formData.UserNameForm,
-        email: formData.UserEmailForm,
-        phone: formData.UserTelForm,
-        country: formData.Aera,
-        service: formData.Service
-      }
-    )
+    postData({
+      name: formData.UserNameForm,
+      email: formData.UserEmailForm,
+      phone: formData.UserTelForm,
+      country: formData.Aera,
+      service: formData.Service,
+      ip: formData.ip,
+      url: formData.url,
+    });
   };
 
   return (
@@ -54,15 +68,12 @@ fetch(`${API}?name=${data.name}&email=${data.email}&phone=${data.phone}&country=
         width: isMobile ? "90%" : globalWidth < 1200 ? "80%" : "",
       }}
     >
-      <div className={`centeredImageForm ${(isMobile ? "mobile": "")}`}>
+      <div className={`centeredImageForm ${isMobile ? "mobile" : ""}`}>
         <img src="/OperaHouseCity.png" alt="Opera House City" />
       </div>
       <div className="centeredForm">
         <div className="formBounding">
-          <form
-            onSubmit={handleFormSubmit}
-            className="InfoForm"
-          >
+          <form onSubmit={handleFormSubmit} className="InfoForm">
             <h2>FILL THE FORM TO GET MORE INFORMATION</h2>
             <input
               type="text"
